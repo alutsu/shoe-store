ac9e678a9dc0538fb34ea50dcdf70de41e3170a2
João Victor <jovictor.miguel@gmail.com>
2022-04-13 10:48:43 -0300

    refactor(api): set default response to json

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  include ActionController::Caching
+
+  before_action :set_default_response_format
+
+  private
+
+  def set_default_response_format
+    request.format = :json
+  end
 end
