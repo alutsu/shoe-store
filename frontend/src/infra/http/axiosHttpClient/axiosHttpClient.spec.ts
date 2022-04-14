bf2fb6bbbca1d81f02bcba1f27b330c6d615e0f5
João Victor <jovictor.miguel@gmail.com>
2022-04-14 11:57:11 -0300

    refactor: move get request creation to a factory method

--- frontend/src/infra/http/axiosHttpClient/axiosHttpClient.spec.ts
+++ frontend/src/infra/http/axiosHttpClient/axiosHttpClient.spec.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import faker from '@faker-js/faker';
+import { HttpGetParams } from 'data/protocols/http/httpGetClient';
 import { AxiosHttpClient } from './axiosHttpClient';
 
 jest.mock('axios');
@@ -7,12 +8,17 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 const makeSut = (): AxiosHttpClient => new AxiosHttpClient();
 
+const mockGetRequest = (): HttpGetParams => ({
+  url: faker.internet.url(),
+  id: faker.datatype.number(100).toString(),
+});
+
 describe('AxiosHttpClient', () => {
   it('should call axios with correct url and verb', async () => {
-    const url = faker.internet.url();
+    const request = mockGetRequest();
     const sut = makeSut();
-    await sut.get({ url });
+    await sut.get(request);
 
-    expect(mockedAxios.get).toHaveBeenCalledWith(url);
+    expect(mockedAxios.get).toHaveBeenCalledWith(request.url);
   });
 });
