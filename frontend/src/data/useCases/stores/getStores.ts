3603bbd2f1075a73d10741a8ccee43d5f986da20
João Victor <jovictor.miguel@gmail.com>
2022-04-13 23:46:23 -0300

    feat: ensure GetStores calls HttpGetClient with correct URL

---
+++ frontend/src/data/useCases/stores/getStores.ts
@@ -0,0 +1,12 @@
+import { HttpGetClient } from 'data/protocols/http/httpGetClient';
+
+export class GetStores {
+  constructor(
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient,
+  ) {}
+
+  async index(): Promise<void> {
+    await this.httpGetClient.get(this.url);
+  }
+}
