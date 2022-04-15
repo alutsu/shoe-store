3ab0972de428569c164484bc756601383b2509ef
João Victor <jovictor.miguel@gmail.com>
2022-04-14 22:25:40 -0300

    refactor: change store events board

--- frontend/src/presentation/components/StoreEventItem/index.tsx
+++ frontend/src/presentation/components/StoreEventItem/index.tsx
@@ -14,20 +14,60 @@ export type StoreEvent = {
 
 type Props = {
   store_event: StoreEvent;
+  eventIdx: number;
+  numberOfEvents: number;
 };
 
-const StoreEventItem: React.FC<Props> = ({ store_event }: Props) => (
-  <tr key={store_event.id}>
-    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
-      {store_event.name}
-    </td>
-    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-      {store_event.change}
-    </td>
-    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-      {store_event.inventory}
-    </td>
-  </tr>
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
+const StoreEventItem: React.FC<Props> = ({
+  store_event,
+  eventIdx,
+  numberOfEvents,
+}: Props) => (
+  <li key={store_event.id}>
+    <div className="relative pb-8">
+      {eventIdx !== numberOfEvents - 1 ? (
+        <span
+          className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
+          aria-hidden="true"
+        />
+      ) : null}
+      <div className="relative flex space-x-3">
+        <div>
+          <span
+            className={classNames(
+              store_event.event_color,
+              'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white',
+            )}
+          >
+            {store_event.event_icon}
+          </span>
+        </div>
+        <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+          <div className="flex flex-col">
+            <p className="text-sm text-gray-500 font-semibold">
+              {store_event.store}
+            </p>
+            <div className="flex">
+              <p className="text-sm text-gray-500 mr-4">
+                {store_event.shoe_model}
+              </p>
+              <p className="text-sm text-gray-500">
+                {store_event.change > 0 ? 'Restocked' : 'Sold'}{' '}
+                {Math.abs(store_event.change)}
+              </p>
+            </div>
+          </div>
+          <div className="text-right text-sm whitespace-nowrap text-gray-500">
+            <time>{store_event.event_time}</time>
+          </div>
+        </div>
+      </div>
+    </div>
+  </li>
 );
 
 export default StoreEventItem;
--- frontend/src/presentation/components/StoreEvents/index.tsx
+++ frontend/src/presentation/components/StoreEvents/index.tsx
@@ -1,10 +1,6 @@
 import { StoreEventsContext } from 'presentation/context/store/getStoreEvent';
 import React, { useContext } from 'react';
-// import StoreEventItem from '../StoreEventItem';
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
-}
+import StoreEventItem from '../StoreEventItem';
 
 const StoreEvents: React.FC = () => {
   const { storeEvents } = useContext(StoreEventsContext);
@@ -12,47 +8,12 @@ const StoreEvents: React.FC = () => {
     <div className="flex justify-center mt-10">
       <ul className="-mb-8">
         {storeEvents.map((event, eventIdx) => (
-          <li key={event.id}>
-            <div className="relative pb-8">
-              {eventIdx !== storeEvents.length - 1 ? (
-                <span
-                  className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                  aria-hidden="true"
-                />
-              ) : null}
-              <div className="relative flex space-x-3">
-                <div>
-                  <span
-                    className={classNames(
-                      event.event_color,
-                      'h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white',
-                    )}
-                  >
-                    {event.event_icon}
-                  </span>
-                </div>
-                <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-                  <div className="flex flex-col">
-                    <p className="text-sm text-gray-500 font-semibold">
-                      {event.store}
-                    </p>
-                    <div className="flex">
-                      <p className="text-sm text-gray-500 mr-4">
-                        {event.shoe_model}
-                      </p>
-                      <p className="text-sm text-gray-500">
-                        {event.change > 0 ? 'Restocked' : 'Sold'}{' '}
-                        {Math.abs(event.change)}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="text-right text-sm whitespace-nowrap text-gray-500">
-                    <time>{event.event_time}</time>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
+          <StoreEventItem
+            key={event.id}
+            store_event={event}
+            eventIdx={eventIdx}
+            numberOfEvents={storeEvents.length}
+          />
         ))}
       </ul>
     </div>
